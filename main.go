64ffdb37c724bc1311eb00be84bf46155ca9e937
mooyg <mooybot@gmail.com>
2024-07-27 20:30:49 +0530

    feat:  118s on single thread M2 8gb

--- main.go
+++ main.go
@@ -12,9 +12,7 @@ import (
 
 func main() {
 
-	mapOfTemp := readFileIntoMap("./measurements.txt")
-
-	log.Print(mapOfTemp)
+	readFileIntoMap("./measurements.txt")
 
 }
 
@@ -23,6 +21,7 @@ type CityTemp struct {
 	max     float64
 	mean    float64
 	visited int
+	sum     float64
 }
 
 type CityMap = map[string]CityTemp
@@ -38,27 +37,33 @@ func readFileIntoMap(filepath string) CityMap {
 
 	r := bufio.NewReader(f)
 
+	i := 0
 	for {
+		i++
 		line, err := r.ReadString('\n')
 
 		if err != nil {
 			if err == io.EOF {
 				return mapOfTemp
 			}
-
 			log.Fatal("some error occured", err)
 		}
+
 		index := strings.Index(line, ";")
 		city := line[:index]
 		temp := line[index+1 : len(line)-1]
 
 		num, err := strconv.ParseFloat(temp, 64)
 
-		log.Println(mapOfTemp[city])
+		if i%10000 == 0 {
+			log.Println("at count", i)
+		}
+
 		if err != nil {
 			log.Fatal("string conversion failed", err)
 
 		}
+
 		if _, ok := mapOfTemp[city]; ok {
 
 			current := mapOfTemp[city]
@@ -67,15 +72,17 @@ func readFileIntoMap(filepath string) CityMap {
 				min:     math.Min(current.min, num),
 				max:     math.Max(current.max, num),
 				visited: current.visited + 1,
-				mean:    current.mean + num/(float64(current.visited)+1),
+				mean:    current.sum/float64(current.visited) + 1,
+				sum:     current.sum + num,
 			}
 
 		} else {
 			mapOfTemp[city] = CityTemp{
 				min:     math.Inf(1),
 				max:     math.Inf(-1),
-				mean:    num / 1,
+				mean:    num,
 				visited: 1,
+				sum:     num,
 			}
 		}
 	}
